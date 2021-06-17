765d694d2848dde5182d1ef1a71dba29e6b0f949
elliottyan <elliottyan@tencent.com>
2021-06-17 10:24:47 +0800

    add more scripts

---
+++ ende_scripts/train_bart_fill_kd_alpha0.75_bart-decoder-init_d2.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+src=en
+tgt=de
+bedropout=0.5
+ARCH=transformer_wmt_en_de
+ROOT=/apdcephfs/share_47076/elliottyan/co-work-projects/fairseq-bert
+
+#### MODIFY ######
+KD_ALPHA=0.75
+DATA_SIG=wmt14_en_de-bert-or-bart
+N_AUTO_DEC_LAYER=2
+MODEL_SIG=d512_bart_fill_kd_bart_decoder_init_d${N_AUTO_DEC_LAYER}_alpha_${KD_ALPHA}
+#### MODIFY ######
+
+DATAPATH=$ROOT/data-bin/$DATA_SIG
+SAVEDIR=$ROOT/checkpoints/$DATA_SIG/$MODEL_SIG
+mkdir -p $SAVEDIR
+
+export CUDA_VISIBLE_DEVICES=0,1,2,3
+# export CUDA_VISIBLE_DEVICES=1
+
+LC_ALL=en_US.UTF-8 python $ROOT/fairseq_cli/train.py $DATAPATH \
+-a $ARCH --optimizer adam --lr 0.0007 -s $src -t $tgt \
+--no-epoch-checkpoints --save-interval-updates 5000 \
+--dropout 0.1 --max-tokens 4000 --lr-scheduler inverse_sqrt --weight-decay 0.0001 \
+--adam-betas '(0.9,0.98)' --save-dir $SAVEDIR --label-smoothing 0.1 \
+--log-interval 100 --disable-validation \
+--fp16 --update-freq 1 --ddp-backend=no_c10d \
+--max-update 200000 --warmup-updates 4000 --warmup-init-lr '1e-07' \
+--criterion new_fill_distillation_loss \
+--denoising --text-filling --use-bartinput --bart-decoder --bart-decoder-init \
+--left-pad-source \
+--bart-auto-bartdecoder $N_AUTO_DEC_LAYER \
+--kd-alpha $KD_ALPHA --bart-model-name $ROOT/pretrain_models/bart-base \
+--bert-model-name $ROOT/pretrain_models/bert-base-cased-new
+
+# --use-bertinput 
+
+# --share-all-embeddings
+# --input-mapping 
+# --text-filling
+# --bart-model-name $ROOT/pretrain_models/bart-base
+# --denoising 
